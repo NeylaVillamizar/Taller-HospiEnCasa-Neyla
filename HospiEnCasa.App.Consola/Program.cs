158b37a48b5b1133b8467934738316f4ef692582
NeylaVillamizar <neylavillamizar@yahoo.es>
2021-09-26 12:43:52 -0500

    Commit solo entidades

---
+++ HospiEnCasa.App.Consola/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
+namespace HospiEnCasa.App.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
